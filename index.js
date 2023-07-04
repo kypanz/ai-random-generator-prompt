2328dcf345ce1493b6b002682fd5696e7aa4c12b
kypanz <kypanz95@gmail.com>
2023-07-04 12:31:23 -0300

    Adding some sex actions recolected from internet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import actions from './actions.js';
 import hobbies from './hobbies.js';
+import sex_actions from './sex_actions.js';
 
 // Generating a random action for your waifu
 function getRandomAction() {
@@ -13,10 +14,17 @@ function getRandomHobby() {
   return hobbies[randomIndex];
 }
 
+// Getting a random hobby from your waifu
+function getRandomSexAction() {
+  const randomIndex = Math.floor(Math.random() * sex_actions.length);
+  return sex_actions[randomIndex];
+}
+
 // Generate a random prompt with specific characteristics
-const characterDescription = "short and light blue hair, and a hoodie";
+const characterDescription = "short and light blue hair, hoodie";
 const randomAction = getRandomAction();
 const randomHobby = getRandomHobby();
-const prompt = `A character with ${characterDescription} is ${randomAction} while ${randomHobby}.`;
-
+const randomSexAction = getRandomSexAction();
+const prompt = `Teen girl with ${characterDescription}, ${randomAction} while ${randomHobby}.`;
+const prompt_sex = `((${randomSexAction})), nude, Teen girl with ${characterDescription}, ((${randomSexAction}))`;
 console.log(prompt);
